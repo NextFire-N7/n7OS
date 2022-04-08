416dfa2781ef41b77b28676d5fd69edea85de309
NextFire <git@yuru.moe>
2022-04-08 17:16:42 +0200

    console: savepoint

--- kernel/console.c
+++ kernel/console.c
@@ -1,5 +1,58 @@
 #include <n7OS/console.h>
+#include <n7OS/cpu.h>
 
-void console_putbytes(const char *s, int len) {
-  
+typedef unsigned char uint8_t;
+
+int cursor_pos = 0;
+
+void console_putcursor()
+{
+    int pos = 0xB8000 + cursor_pos * 2;
+    outb(0xF, 0x3D4);
+    outb(pos & 0xFF, 0x3D5);
+    outb(0xE, 0x3D4);
+    outb(pos >> 0xF, 0x3D5);
+}
+
+void console_putchar(char c)
+{
+
+    switch (c)
+    {
+    case '\b':
+        cursor_pos -= 1;
+        break;
+
+    case '\t':
+        cursor_pos += 8;
+        break;
+
+    case '\n':
+        cursor_pos += 80 - cursor_pos % 80;
+        break;
+
+    case '\f':
+        break;
+
+    case '\r':
+        cursor_pos -= cursor_pos % 80;
+        break;
+
+    default:
+        if (c > 31 && c < 127)
+        {
+            uint8_t *scr_tab = (uint8_t *)0xB8000;
+            int pos = 0xB8000 + cursor_pos * 2;
+            scr_tab[pos] = (0x0F << 8) | c;
+            cursor_pos++;
+        }
+        break;
+    }
+    console_putcursor();
+}
+
+void console_putbytes(const char *s, int len)
+{
+    for (int i = 0; i < len; i++)
+        console_putchar(s[i]);
 }
--- kernel/start.c
+++ kernel/start.c
@@ -1,13 +1,15 @@
 #include <n7OS/cpu.h>
 #include <inttypes.h>
 #include <n7OS/processor_structs.h>
-
+#include <stdio.h>
 
 void kernel_start(void)
 {
-    
+
     // on ne doit jamais sortir de kernel_start
-    while (1) {
+    while (1)
+    {
+        printf("Hello world!\n");
         // cette fonction arrete le processeur
         hlt();
     }
