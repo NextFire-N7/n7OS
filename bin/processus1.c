0b38729bac7a57ffbaf6117c86a7de34b5dd94d5
NextFire <git@yuru.moe>
2022-06-01 12:08:55 +0200

    logging

--- bin/processus1.c
+++ bin/processus1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <n7OS/process.h>
 
-void processus1() {
-  printf("Hello, world from P1\n");
-  for (;;);
+void processus1()
+{
+  for (int i = 0; i < 2; i++)
+  {
+    printf("Hello, world from P1, i=%d\n", i);
+    scheduler();
+  }
+  terminer();
 }
--- kernel/process.c
+++ kernel/process.c
@@ -9,6 +9,7 @@ pid_t pid_counter = 0;
 
 pid_t creer(const char *name, fnptr function)
 {
+    printf("> creer %s\n", name);
     process_t *process;
     for (int i = 0; i < NB_PROC; i++)
     {
@@ -18,6 +19,7 @@ pid_t creer(const char *name, fnptr function)
             process->pid = pid_counter;
             process->state = PRET;
             strcpy(process->name, name);
+            memset(process->stack, 0, sizeof(process->stack));
             process->stack[STACK_SIZE - 1] = (uint32_t)function;
             memset(process->ctx, 0, sizeof(process->ctx));
             process->ctx[ESP] = (uint32_t)&process->stack[STACK_SIZE - 1];
@@ -32,37 +34,43 @@ extern void ctx_sw(void *ctx_old, void *ctx_new);
 
 void scheduler()
 {
+    printf("> scheduler\n");
     pid_t curr_pid = getpid();
     pid_t new_pid = -1;
-    pid_t npid;
     for (pid_t i = 1; i < NB_PROC; i++)
     {
-        npid = (curr_pid + i) % NB_PROC;
+        pid_t npid = (curr_pid + i) % NB_PROC;
         if (process_table[npid].state == PRET)
         {
             new_pid = npid;
             break;
         }
     }
+    printf(">> new pid, name: %i, %s\n", new_pid, process_table[new_pid].name);
     if (new_pid != -1)
     {
         process_table[curr_pid].state = PRET;
         process_table[new_pid].state = ELU;
-        ctx_sw(&process_table[curr_pid].ctx, &process_table[new_pid].ctx);
         current_process = &process_table[new_pid];
+        ctx_sw(&process_table[curr_pid].ctx, &process_table[new_pid].ctx);
+    }
+    else
+    {
+        printf("> scheduler: no process to switch to\n");
     }
 }
 
 void arreter()
 {
+    printf("> arreter %s\n", current_process->name);
     current_process->state = ELU;
     scheduler();
 }
 
 void terminer()
 {
+    printf("> terminer %s\n", current_process->name);
     current_process->state = NONE;
-    memset(current_process->stack, 0, sizeof(current_process->stack));
     scheduler();
 }
 
@@ -73,10 +81,12 @@ pid_t getpid()
 
 void init()
 {
-    printf("Hello, world from init\n");
-    scheduler();
     while (1)
+    {
+        printf("Hello, world from init\n");
+        scheduler();
         hlt();
+    }
 }
 
 extern void processus1();
